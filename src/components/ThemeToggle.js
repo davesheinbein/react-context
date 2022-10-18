fc1d0dd90ccb9a179a7dd12a1ee56e5aa19d8caa
David Sheinbein <davesheinbein@gmail.com>
2022-10-17 18:39:25 -0700

    update bg

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useThemeContextPresenter } from '../context/ThemeContextPresenter';
 
 const ThemeToggle = () => {
+	const [randomColor, setRandomColor] = useState(
+		'#' + Math.floor(Math.random() * 0xffffff).toString(16)
+	);
 	const { isDarkTheme, light, dark, toggleTheme } =
 		useThemeContextPresenter();
 	const theme = isDarkTheme ? dark : light;
+	useEffect(() => {
+		setRandomColor(
+			'#' +
+				Math.floor(Math.random() * 0xffffff).toString(16)
+		);
+		document.getElementById('root').style.background =
+			randomColor;
+	}, [isDarkTheme]);
+
 	return (
 		<div
 			className='theme-toggle'
